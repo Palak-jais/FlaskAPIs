5fe98b52e746ba05327449fcf713dd1808c63601
palak jaiswal <palakjaiswalch28@gmail.com>
2023-10-09 22:37:36 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+app.static_folder = 'static'
+
+@app.route('/') #Decorator
+def home_page():
+    return render_template('home_page.html')
+
+
+@app.route('/about') #Decorator
+def about_page():
+    return render_template('about_page.html')
+
+@app.route('/expense') #Decorator
+def expense_page():
+    item=[
+        {"id":1,"saving":2000,"item":"Car"},
+           {"id":2,"saving":89000,"item":"Clothes"},
+             {"id":3,"saving":45000,"item":"Pot"},
+               {"id":4,"saving":90000,"item":"Bike"},
+    ]
+    return render_template('expense.html',data_value=item)
